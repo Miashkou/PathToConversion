aacf0c6d075b114f1b28fc20a8463f1ac1f7f4e8
Vykintas Burbulis <vykintas.burbulis@gmail.com>
2016-09-29 17:32:21 +0300

    removed unused code

--- PathToConversion/Transaction.cs
+++ PathToConversion/Transaction.cs
@@ -13,33 +13,5 @@ namespace PathToConversion
         public string URLfrom { get; set; }
         public string ID_LogPoints { get; set; }
         public string LogPointName { get; set; }
-
-        // Add equals ovveride
-        public override bool Equals(object obj)
-        {
-            return LogTime.Equals(obj);
-        }
-
-        protected bool Equals(Transaction other)
-        {
-            return LogTime.Equals(other.LogTime) && TransactionType == other.TransactionType && CookieId == other.CookieId && string.Equals(Campaign, other.Campaign) && string.Equals(Media, other.Media) && string.Equals(Banner, other.Banner) && string.Equals(URLfrom, other.URLfrom) && string.Equals(ID_LogPoints, other.ID_LogPoints) && string.Equals(LogPointName, other.LogPointName);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = LogTime.GetHashCode();
-                hashCode = (hashCode*397) ^ TransactionType;
-                hashCode = (hashCode*397) ^ CookieId;
-                hashCode = (hashCode*397) ^ (Campaign != null ? Campaign.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (Media != null ? Media.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (Banner != null ? Banner.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (URLfrom != null ? URLfrom.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (ID_LogPoints != null ? ID_LogPoints.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (LogPointName != null ? LogPointName.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
     }
 }
