dc5dbca232a7b11d373e58383ad6d636692d615b
Vykintas Burbulis <vykintas.burbulis@gmail.com>
2016-10-03 14:30:30 +0300

    Switched location of json files

--- PathToConversion/Program.cs
+++ PathToConversion/Program.cs
@@ -6,6 +6,18 @@ namespace PathToConversion
     {
         public static void Main()
         { 
+            //Enter one of json files to getFileInformation:
+            //1)adInteractionTransactions
+            //2)deadCookieTransactions
+            //3)defaultTransactions
+            //4)doubleTransactions
+            //5)FailuteTransactions
+            //6)mediaPathTransactions
+            //7)referringTransactions
+            //8)successTransactions
+            //9)testClient
+            //10)transactions
+            //11)bigdata
             Filter.GetSuccessfulTransaction(DataReader.GetFileInformation("bigdata"));
             Console.Read();
         }
